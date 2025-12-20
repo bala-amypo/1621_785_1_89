64e565d5447fabc1311c33934b249d8f121ff594
bala-amypo <bala@amypo.in>
2025-12-20 10:45:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -11,3 +11,14 @@
 // }
 
 
+package com.example.demo.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
