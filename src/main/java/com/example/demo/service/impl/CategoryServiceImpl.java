8a00537d55cb781afc503f42590ae665541158cd
bala-amypo <bala@amypo.in>
2025-12-20 16:10:46 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -1,33 +1,33 @@
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
-// import org.springframework.stereotype.Service;
-// import com.example.demo.service.CategoryService;
-// import com.example.demo.model.Category;
-// import com.example.demo.repository.CategoryRepository;
-// import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example.demo.service.CategoryService;
+import com.example.demo.model.Category;
+import com.example.demo.repository.CategoryRepository;
+import java.util.List;
 
-// @Service
-// public class CategoryServiceImpl implements CategoryService {
+@Service
+public class CategoryServiceImpl implements CategoryService {
 
-//     private final CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
 
-//     public CategoryServiceImpl(CategoryRepository categoryRepository) {
-//         this.categoryRepository = categoryRepository;
-//     }
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
-//     @Override
-//     public Category createCategory(Category category) {
-//         return categoryRepository.save(category);
-//     }
+    @Override
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
 
-//     @Override
-//     public Category getCategory(Long id) {
-//         return categoryRepository.findById(id).orElse(null);
-//     }
+    @Override
+    public Category getCategory(Long id) {
+        return categoryRepository.findById(id).orElse(null);
+    }
 
-//     @Override
-//     public List<Category> getAllCategories() {
-//         return categoryRepository.findAll();
-//     }
-// }
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+}
 
