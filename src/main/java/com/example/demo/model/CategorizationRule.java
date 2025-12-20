c71f2b73737a965244e21d26da17fe9c6f35450b
bala-amypo <bala@amypo.in>
2025-12-20 11:18:32 +0000

    User commit

--- src/main/java/com/example/demo/model/CategorizationRule.java
+++ src/main/java/com/example/demo/model/CategorizationRule.java
@@ -1,7 +1,5 @@
 package com.example.demo.model;
-
 import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
@@ -23,10 +21,8 @@ public class CategorizationRule {
     private String matchType;
     private Integer priority;
     private LocalDateTime createdAt;
-
     public CategorizationRule() {
     }
-
     public CategorizationRule(Long id, Category category,String keyword, String matchType,Integer priority) {
         this.id = id;
         this.category = category;
@@ -39,27 +35,40 @@ public class CategorizationRule {
     public void onCreate() {
         this.createdAt = LocalDateTime.now();
     }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public Category getCategory() { return category; }
+    public Long getId() {
+         return id;
+          }
+    public void setId(Long id) { 
+        this.id = id;
+         }
+    public Category getCategory() { 
+    return category;
+     }
     public void setCategory(Category category) {
         this.category = category;
     }
+    public String getKeyword() { 
+    return keyword;
+     }
+    public void setKeyword(String keyword) { 
+        this.keyword = keyword;
+         }
 
-    public String getKeyword() { return keyword; }
-    public void setKeyword(String keyword) { this.keyword = keyword; }
-
-    public String getMatchType() { return matchType; }
+    public String getMatchType() { 
+        return matchType;
+         }
     public void setMatchType(String matchType) {
         this.matchType = matchType;
     }
 
-    public Integer getPriority() { return priority; }
+    public Integer getPriority() {
+         return priority; 
+         }
     public void setPriority(Integer priority) {
         this.priority = priority;
     }
 
-    public LocalDateTime getCreatedAt() { return createdAt; }
+    public LocalDateTime getCreatedAt() { 
+        return createdAt; 
+        }
 }
