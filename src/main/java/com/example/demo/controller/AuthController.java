d8ffc16b5fbce782d920ba98c13e71201a788c4f
bala-amypo <bala@amypo.in>
2025-12-21 05:44:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -13,20 +13,21 @@ public class AuthController {
 
     private final UserService userService;
 
-    // Constructor injection
-    public AuthController(UserService userService) {
-        this.userService = userService;
-    }
+        // Constructor injection
+            public AuthController(UserService userService) {
+                    this.userService = userService;
+                        }
 
-    // POST /auth/register
-    @PostMapping("/register")
-    public User registerUser(@RequestBody User user) {
-        return userService.registerUser(user);
-    }
+                            // POST /auth/register
+                                @PostMapping("/register")
+                                    public User registerUser(@RequestBody User user) {
+                                            return userService.registerUser(user);
+                                                }
 
-    // POST /auth/login
-    @PostMapping("/login")
-    public User loginUser(@RequestBody User user) {
-        return userService.findByEmail(user.getEmail());
-    }
-}
+                                                    // POST /auth/login
+                                                        @PostMapping("/login")
+                                                            public User loginUser(@RequestBody User user) {
+                                                                    return userService.findByEmail(user.getEmail());
+                                                                        }
+                                                                        }
+                                                                        
