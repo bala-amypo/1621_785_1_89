cee96f8aec69751197b8985209e88d0c91ad792e
bala-amypo <bala@amypo.in>
2025-12-26 14:23:41 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,51 +1,4 @@
-// package com.example.demo.service.impl;
-// import org.springframework.stereotype.Service;
-// import com.example.demo.service.UserService;
-// import com.example.demo.model.User;
-// import com.example.demo.repository.UserRepository;
-// import com.example.demo.exception.ResourceNotFoundException;
-// import java.util.List;
-// @Service
-// public class UserServiceImpl implements UserService {
-
-//     private final UserRepository userRepository;
-
-//     public UserServiceImpl(UserRepository userRepository) {
-//         this.userRepository = userRepository;
-//     }
-
-//     @Override
-//     public User registerUser(User user) {
-//         if(userRepository.existsById(user.getId())){
-//             throw new ResourceNotFoundException("UserId already exists");
-//         }
-//         return userRepository.save(user);
-//     }
-
-//     @Override
-//     public User findByEmail(String email) {
-//         return userRepository.findByEmail(email).orElse(null);
-//     }
-
-//     @Override
-//     public List<User> getAllUsers() {
-//         return userRepository.findAll();
-//     }
-//     @Override
-//     public User getUserById(Long id) {
-//     return userRepository.findById(id).orElse(null);
-// }
-
-
-// }
-
-
-
-
-
-
 package com.example.demo.service.impl;
-
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.exception.ResourceNotFoundException;
@@ -54,7 +7,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
-
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
