4b5f8afe874610f0e784f41b99571c0dca58260b
bala-amypo <bala@amypo.in>
2025-12-26 08:51:10 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
+++ src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
@@ -1,9 +1,12 @@
-package com.example.demo.repository;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.CategorizationRule;
+// package com.example.demo.repository;
+// import java.util.List;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import com.example.demo.model.CategorizationRule;
 
-public interface CategorizationRuleRepository extends JpaRepository<CategorizationRule, Long> {
+// public interface CategorizationRuleRepository extends JpaRepository<CategorizationRule, Long> {
 
-    List<CategorizationRule> findByCategoryId(Long categoryId);
-}
+//     List<CategorizationRule> findByCategoryId(Long categoryId);
+// }
+
+
+
--- src/main/java/com/example/demo/repository/InvoiceRepository.java
+++ src/main/java/com/example/demo/repository/InvoiceRepository.java
@@ -1,11 +1,11 @@
-package com.example.demo.repository;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.demo.model.Invoice;
-import com.example.demo.model.User;
-@Repository
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+// package com.example.demo.repository;
+// import java.util.List;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+// import com.example.demo.model.Invoice;
+// import com.example.demo.model.User;
+// @Repository
+// public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
 
- }
+//  }
