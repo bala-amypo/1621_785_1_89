ed6568d509f40c24c6252a6deec17101bf84ca7a
bala-amypo <bala@amypo.in>
2025-12-26 11:16:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/InvoiceRepository.java
+++ src/main/java/com/example/demo/repository/InvoiceRepository.java
@@ -13,5 +13,23 @@
 
 
 
+package com.example.demo.repository;
+
+import com.example.demo.model.Invoice;
+import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+    // Test-case exact method names
+    List<Invoice> findByUploadedBy(User user);
+
+    @Query("SELECT i FROM Invoice i WHERE i.amount > :amount")
+    List<Invoice> findByAmountGreaterThanHql(Double amount);
+
+    boolean existsByVendorIdAndInvoiceNumber(Long vendorId, String invoiceNumber);
+}
 
 
--- src/main/java/com/example/demo/repository/VendorRepository.java
+++ src/main/java/com/example/demo/repository/VendorRepository.java
@@ -7,3 +7,17 @@
 // @Repository
 // public interface VendorRepository extends JpaRepository<Vendor, Long> {
 // }
+
+
+
+
+
+package com.example.demo.repository;
+
+import com.example.demo.model.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+
+    boolean existsByVendorName(String vendorName);
+}
