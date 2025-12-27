303a1e96122a784653432f0c1cf2bffe658b389e
bala-amypo <bala@amypo.in>
2025-12-27 14:05:24 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,93 +1,10 @@
-// package com.example.demo.model;
-// import jakarta.persistence.*;
-// import jakarta.validation.constraints.Email;
-// import jakarta.validation.constraints.NotBlank;
-// import java.time.LocalDateTime;
-// import java.util.*;
-// @Entity
-// @Table(
-//     name = "users",
-//     uniqueConstraints = {
-//         @UniqueConstraint(columnNames = "email")
-//     }
-// )
-// public class User {
-//     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-//     @NotBlank
-//     private String fullName;
-//     @Email @NotBlank
-//     @Column(unique = true, nullable = false)
-//     private String email;
-//     @NotBlank
-//     private String password;
-//     @NotBlank
-//     private String role;
-//     private LocalDateTime createdAt;
-//     @ManyToMany
-//     @JoinTable(
-//         name = "user_favorite_vendors",
-//         joinColumns = @JoinColumn(name = "user_id"),
-//         inverseJoinColumns = @JoinColumn(name = "vendor_id")
-//     )
-//     private Set<Vendor> favoriteVendors = new HashSet<>();
-//     @OneToMany(mappedBy = "uploadedBy")
-//     private List<Invoice> invoices; 
-//     public User() {}
-//     public User(String fullName, String email, String password, String role,Set<Vendor> favoriteVendors) {
-//         this.fullName = fullName;
-//         this.email = email;
-//         this.password = password;
-//         this.role = role;
-//         this.favoriteVendors=favoriteVendors;
-//     }
-
-//     @PrePersist
-//     public void prePersist() {
-//         this.createdAt = LocalDateTime.now();
-//         if (this.role == null) this.role = "USER";
-//     }
-
-//     public Long getId() { return id; }
-//     public void setId(Long id) { this.id = id; }
-
-//     public String getFullName() { return fullName; }
-//     public void setFullName(String fullName) { this.fullName = fullName; }
-
-//     public String getEmail() { return email; }
-//     public void setEmail(String email) { this.email = email; }
-
-//     public String getPassword() { return password; }
-//     public void setPassword(String password) { this.password = password; }
-
-//     public String getRole() { return role; }
-//     public void setRole(String role) { this.role = role; }
-
-//     public LocalDateTime getCreatedAt() { return createdAt; }
-//     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
-//     public Set<Vendor> getFavoriteVendors() {
-//         return favoriteVendors;
-//     }
-//     public List<Invoice> getInvoices() { return invoices; }
-//     public void setInvoices(List<Invoice> invoices) { this.invoices = invoices; }
-// }
-
-
-
-
-
-
-
 package com.example.demo.model;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-
 import java.time.LocalDateTime;
 import java.util.*;
-
 @Entity
 @Table(
     name = "users",
