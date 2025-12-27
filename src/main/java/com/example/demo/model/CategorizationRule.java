9f4cb8077790d73224da93e91389c2d9cfb9904e
bala-amypo <bala@amypo.in>
2025-12-27 14:04:36 +0000

    User commit

--- src/main/java/com/example/demo/model/CategorizationRule.java
+++ src/main/java/com/example/demo/model/CategorizationRule.java
@@ -1,84 +1,9 @@
-// package com.example.demo.model;
-// import jakarta.persistence.*;
-// import java.time.LocalDateTime;
-// import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-// import jakarta.persistence.JoinColumn;
-
-
-// @Entity
-// public class CategorizationRule {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-//     private String keyword;
-//     private String matchType;
-//     private Integer priority;
-//     @ManyToOne
-//     @JoinColumn(name = "category_id")
-//   //  @JsonIgnore
-//     private Category category;
-//     private LocalDateTime createdAt;
-//     @PrePersist
-//     public void prePersist() {
-//         this.createdAt = LocalDateTime.now();
-//     }
-
-//     public Long getId() {
-//         return id;
-//     }
-
-//     public String getKeyword() {
-//         return keyword;
-//     }
-
-//     public void setKeyword(String keyword) {
-//         this.keyword = keyword;
-//     }
-
-//     public Integer getPriority() {
-//         return priority;
-//     }
-
-//     public void setPriority(Integer priority) {
-//         this.priority = priority;
-//     }
-
-//     public String getMatchType() {
-//         return matchType;
-//     }
-
-//     public void setMatchType(String matchType) {
-//         this.matchType = matchType;
-//     }
-
-//     public Category getCategory() {
-//         return category;
-//     }
-
-//     public void setCategory(Category category) {
-//         this.category = category;
-//     }
-
-//     public LocalDateTime getCreatedAt() {
-//         return createdAt;
-//     }
-// }
-
-
-
-
-
-
 package com.example.demo.model;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
-
 @Entity
 public class CategorizationRule {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
