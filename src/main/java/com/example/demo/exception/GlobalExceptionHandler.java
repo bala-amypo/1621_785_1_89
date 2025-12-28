7b8b4f7a9a4fbca3d399eceb7edb5444df581f8a
bala-amypo <bala@amypo.in>
2025-12-28 18:25:45 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,61 +1,22 @@
-// package com.example.demo.exception;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
-// import org.springframework.web.bind.annotation.RestControllerAdvice;
-// @RestControllerAdvice
-// public class GlobalExceptionHandler {   
-//     @ExceptionHandler(ResourceNotFoundException.class)
-//     public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException ex) {
-//         ApiError error = new ApiError(HttpStatus.NOT_FOUND.value(), ex.getMessage());
-//         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-//     }
- 
-//     @ExceptionHandler(Exception.class)
-//     public ResponseEntity<ApiError> handleException(Exception ex) {
-//         ApiError error = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
-//         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
-//     }
-// }
-
-
 package com.example.demo.exception;
-
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 @RestControllerAdvice
-public class GlobalExceptionHandler {
-
+public class GlobalExceptionHandler {   
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException ex) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("X-Exception-Type", "ResourceNotFoundException");
-
-        ApiError error = new ApiError(
-                HttpStatus.NOT_FOUND.value(),
-                ex.getMessage()
-        );
-
-        return new ResponseEntity<>(error, headers, HttpStatus.NOT_FOUND);
+        ApiError error = new ApiError(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
-
+ 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiError> handleException(Exception ex) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("X-Exception-Type", ex.getClass().getSimpleName());
-
-        ApiError error = new ApiError(
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                ex.getMessage()
-        );
-
-        return new ResponseEntity<>(error, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+        ApiError error = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
+
+
 
